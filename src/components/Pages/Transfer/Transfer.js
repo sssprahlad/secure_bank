77566fbe628242e28ee6ceaa425340897dde4410
sssprahlad <k.sssprahlad@gmail.com>
2026-05-15 09:12:30 +0530

    new updated changes

--- src/components/Pages/Transfer/Transfer.js
+++ src/components/Pages/Transfer/Transfer.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { API_BASE_URL } from "../../../Constants/Constants";
 import './Transfer.css';
 
 
 
 const Transfer = () => {
+    const navigator = useNavigate();
     const [user, setUser] = useState(null);
     const [selectedFromUser, setSelectedFromUser] = useState(null);
     const [selectedToUser, setSelectedToUser] = useState(null);
@@ -14,6 +16,7 @@ const Transfer = () => {
         transferAmount: 0
 
     });
+    const [paymentSuccess, setPaymentSuccess] = useState(false);
 
 
     console.log(user);
@@ -37,6 +40,23 @@ const Transfer = () => {
         fetchAllBankCustomers();
     }, []);
 
+    useEffect(() => {
+        if (paymentSuccess) {
+            setTimeout(() => {
+                setPaymentSuccess(false);
+                setSelectedFromUser(null);
+                setSelectedToUser(null);
+                setTransferDetails({
+                    selectedFromAccount: null,
+                    selectedToAccount: null,
+                    transferAmount: 0
+                });
+                navigator("/accounts")
+            }, 3000);
+        }
+    }, [paymentSuccess]);
+    
+
     const handleTransferAmount = async () => {
         console.log("Transfer Amount", transferDetails);
         try {
@@ -50,9 +70,12 @@ const Transfer = () => {
                     body: JSON.stringify(transferDetails),
                 }
             );
-            const data = await response.json();
-            console.log(data);
-
+            
+            console.log(response, "transfer");
+            if(response.ok){
+                setPaymentSuccess(true);
+            }
+            
         } catch (error) {
             console.log(error);
         }
@@ -62,7 +85,7 @@ const Transfer = () => {
     return (
         <div className="transfer">
             <h1>Transfer Amount</h1>
-            <form className="transfer-form">
+                <form className="transfer-form">
                 <div className="form-group">
                     <label htmlFor="fromAccount">From Account :- {selectedFromUser?.accountHolderName}</label>
                     <select id="fromAccount" onChange={(e) => {
@@ -84,7 +107,7 @@ const Transfer = () => {
                     <label htmlFor="toAccount">To Account :- {selectedToUser?.accountHolderName}</label>
                     <select id="toAccount" onChange={(e) => {
                         const selectId = e.target.value;
-                        const selectedUser = user.find((user) => user.id === selectId);
+                        const selectedUser = user.find((user) => user.id === selectId); 
                         setSelectedToUser(selectedUser);
                         setTransferDetails({ ...transferDetails, selectedToAccount: selectId })
                     }} value={selectedToUser?.id}>
@@ -99,11 +122,11 @@ const Transfer = () => {
                 </div>
                 <div className="form-group">
                     <label htmlFor="amount">Amount  {transferDetails?.transferAmount ? `: ${transferDetails?.transferAmount}` : ''}</label>
-                    <input type="number" id="amount" placeholder="Enter amount" onChange={(e) => setTransferDetails({ ...transferDetails, transferAmount: e.target.value })} />
+                    <input type="number" id="amount" placeholder="Enter amount" onChange={(e) => setTransferDetails({ ...transferDetails, transferAmount: e.target.value })} required/>
                 </div>
-                <button type="button" className="transfer-btn" onClick={() => handleTransferAmount()}>Transfer</button>
+                <button type="button" className="transfer-btn" disabled={!transferDetails?.selectedFromAccount || !transferDetails?.selectedToAccount || !transferDetails?.transferAmount} onClick={() => handleTransferAmount()}>Transfer</button>
             </form>
-
+            {paymentSuccess && <div className="payment-success">Payment transferred Successfully</div>}
         </div>
     )
 }
