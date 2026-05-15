de1b993b6888162be88ad12bbd2b0af1c8b58d32
sssprahlad <k.sssprahlad@gmail.com>
2026-05-15 12:36:19 +0530

    new updated changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,11 +19,11 @@ const Header = () => {
 
                 <nav className={`nav ${isMenuOpen ? 'nav-open' : ''}`}>
                     <ul className="nav-list">
-                      <li><Link to="/" className={location.pathname === '/' ? 'active' : ''}>Home</Link></li>
-                      <li><Link to="/accounts" className={location.pathname === '/accounts' ? 'active' : ''}>Accounts</Link></li>
-                      <li><Link to="/money-transfer" className={location.pathname === '/money-transfer' ? 'active' : ''}>Transfer</Link></li>
-                      <li><Link to="/history" className={location.pathname === '/history' ? 'active' : ''}>History</Link></li>
-                      <li><Link to="/customer-support" className={location.pathname === '/customer-support' ? 'active' : ''}>Support</Link></li>
+                      <li><Link to="/" onClick={() => setIsMenuOpen(false)} className={location.pathname === '/' ? 'active' : ''}>Home</Link></li>
+                      <li><Link to="/accounts" onClick={() => setIsMenuOpen(false)} className={location.pathname === '/accounts' ? 'active' : ''}>Accounts</Link></li>
+                      <li><Link to="/money-transfer" onClick={() => setIsMenuOpen(false)} className={location.pathname === '/money-transfer' ? 'active' : ''}>Transfer</Link></li>
+                      <li><Link to="/history" onClick={() => setIsMenuOpen(false)} className={location.pathname === '/history' ? 'active' : ''}>History</Link></li>
+                      <li><Link to="/customer-support" onClick={() => setIsMenuOpen(false)} className={location.pathname === '/customer-support' ? 'active' : ''}>Support</Link></li>
                     </ul>
                 </nav>
 
